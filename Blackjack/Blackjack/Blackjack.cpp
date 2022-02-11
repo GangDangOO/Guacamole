0e2efa82fdf437b47f4db4aaaa08618bbd402746
GangDangOO <dnj456@gmail.com>
2022-02-11 16:23:23 +0900

    2022-02-11

---
+++ Blackjack/Blackjack/Blackjack.cpp
@@ -0,0 +1,140 @@
+﻿// A는 1 고정이고 J, Q, K는 10으로 고정됨
+// 딜러는 17이상이 될때까지 계속 뽑아야함
+#include <iostream>
+#include <string>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+const int MAX_CARD = 52;
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> dis(0, MAX_CARD - 1);
+
+struct _CARD {
+	string	shape;
+	int		num;
+
+	void Print() {
+		cout << shape;
+		switch (num)
+		{
+		case 1:
+			cout << 'A';
+			break;
+		case 11:
+			cout << 'J';
+			break;
+		case 12:
+			cout << 'Q';
+			break;
+		case 13:
+			cout << 'K';
+			break;
+		default:
+			cout << num;
+			break;
+		}
+	}
+};
+
+struct _PLAYER
+{
+	string name;
+	vector<_CARD> deck;
+	int point = 0;
+	int money = 10000;
+};
+
+void Draw_card_and_print(_CARD card, _PLAYER &player);
+void Init_card(_CARD* cards, int MAX_CARD);
+void Shuffle_card(_CARD* cards);
+
+
+int main() {
+	int input;
+	_CARD cards[MAX_CARD];
+	_PLAYER player;
+	_PLAYER dealer;
+	player.name = "Player 1";
+	dealer.name = "Dealer";
+	Init_card(cards, MAX_CARD);
+	while (true) {
+		Shuffle_card(cards);
+		int i = 0;
+		for (; i < MAX_CARD; i++) {
+			Draw_card_and_print(cards[i], player);
+			if (player.point > 21) {
+				cout << "꽝" << endl;
+				player.point = 0;
+				break;
+			}
+			cout << "카드뽑기 1.예 2.아니오";
+			cin >> input;
+			if (input == 2) break;
+		}
+		for (; i < MAX_CARD; i++) {
+			Draw_card_and_print(cards[i], dealer);
+			if (player.point > 21) {
+				cout << "꽝" << endl;
+				dealer.point = 0;
+				break;
+			}
+			if (dealer.point > 17) break;
+		}
+		player.money -= 1000;
+		if (player.point == dealer.point) {
+			cout << "무승부" << endl << "현재소지금 : " << player.money << endl;
+		} 
+		else if (player.point > dealer.point) {
+			player.money += 2000;
+			cout << "플레이어 승리! 소지금 + 2000" << endl << "현재소지금 : " << player.money << endl;
+		}
+		else {
+			cout << "플레이어 패배 소지금 - 1000 " << endl << " 현재소지금 : " << player.money << endl;
+		}
+		player.deck.clear();
+		dealer.deck.clear();
+	}
+	return 0;
+}
+
+void Draw_card_and_print(_CARD card, _PLAYER &player) {
+	player.point = 0;
+	player.deck.push_back(card);
+	cout << player.name << ' ';
+	for (int i = 0; i < player.deck.size(); i++) {
+		player.deck[i].Print();
+		cout << '\t';
+		if (player.deck[i].num > 10) player.point += 10;
+		else player.point += player.deck[i].num;
+	}
+	cout << player.point << endl;
+}
+
+void Init_card(_CARD* cards, int MAX_CARD) {
+	for (int i = 0; i < MAX_CARD / 4; i++) {
+		cards[i].shape = "♠";
+		cards[i].num = i + 1;
+		cards[i + 13].shape = "♥";
+		cards[i + 13].num = i + 1;
+		cards[i + 26].shape = "♣";
+		cards[i + 26].num = i + 1;
+		cards[i + 39].shape = "◈";
+		cards[i + 39].num = i + 1;
+	}
+
+}
+
+void Shuffle_card(_CARD* cards) {
+	for (int i = 0; i < 200; i++) {
+		_CARD temp;
+		int rd1 = dis(gen);
+		int rd2 = dis(gen);
+		temp = cards[rd1];
+		cards[rd1] = cards[rd2];
+		cards[rd2] = temp;
+	}
+}
---
+++ Vector/Vector/Vector.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int size = 20;
+	vector<int> v1;
+	for (int i = 0; i < size; i++) {
+		v1.push_back(i);
+		cout << v1[i] << endl;
+		cout << "사이즈" << v1.size() << endl;
+		cout << "메모리" << v1.capacity() << endl << endl;
+	}
+		v1.insert(v1.begin(), 100);
+		cout << v1[0] << endl;
+		cout << *v1.begin() << endl;
+		cout << v1.front() << endl;
+		cout << v1.back() << endl;
+		vector<int>().swap(v1);
+		v1.resize(20);
+		cout << "사이즈" << v1.size() << endl;
+		cout << "메모리" << v1.capacity() << endl << endl;
+		v1.push_back(0);
+		cout << "사이즈" << v1.size() << endl;
+		cout << "메모리" << v1.capacity() << endl << endl;
+		v1.insert(v1.begin() + 20, 5);
+		v1.shrink_to_fit();
+		cout << "사이즈" << v1.size() << endl;
+		cout << "메모리" << v1.capacity() << endl << endl;
+
+
+}
